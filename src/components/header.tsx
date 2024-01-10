cdbb9ccd612c41a7bf3a9e08db556b3cf173e7ca
Harysson <haryssonsoares@gmail.com>
2024-01-10 18:46:27 -0300

    feat: add sheet at header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,23 @@
 import Image from "next/image";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { MenuIcon } from "lucide-react";
 
 export const Header = () => {
   return (
-    <div className="p-3">
+    <div className="p-3 flex justify-between items-center">
       <Image
         src="./logo.svg"
         alt="Logo of Netimóveis"
         width={126}
         height={36}
       />
+
+      <Sheet>
+        <SheetTrigger asChild>
+          <MenuIcon className="text-[#4E5254]" />
+        </SheetTrigger>
+        <SheetContent side="right">MENU</SheetContent>
+      </Sheet>
     </div>
   );
 };
