54193e53827b87245042721204241c213226965b
Harysson <haryssonsoares@gmail.com>
2024-01-11 13:53:48 -0300

    improvment: add title, subtitle and gradient to places section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { AdsSection } from "@/components/domain/home/section/ads";
 import { Banner } from "@/components/domain/home/section/banner";
+import { PlacesSection } from "@/components/domain/home/section/places";
 import { TabsSection } from "@/components/domain/home/section/tabs";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <Banner />
       <TabsSection />
       <AdsSection />
+      <PlacesSection />
     </div>
   );
 }
