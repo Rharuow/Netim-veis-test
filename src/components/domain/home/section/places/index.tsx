d1ac632e013fcbf7f928f30ad68aeec3479716f9
Harysson <haryssonsoares@gmail.com>
2024-01-11 18:17:20 -0300

    improvment: create separator component

--- src/components/domain/home/section/places/index.tsx
+++ src/components/domain/home/section/places/index.tsx
@@ -1,10 +1,12 @@
 "use client";
+import { Separator } from "@radix-ui/react-separator";
 import { Topic } from "./topic";
+import { Tabs } from "@/components/ui/tabs";
 
 export const PlacesSection = () => {
   return (
-    <div className="flex flex-col px-4 gap-12 bg-gradient-to-b from-[#EDF1F4] to-white pt-16">
-      <div className="flex flex-col gap-6">
+    <div className="flex flex-col gap-12 bg-gradient-to-b from-[#EDF1F4] to-white pt-16">
+      <div className="flex flex-col gap-6  px-4">
         <h2 className="text-3xl font-bold">
           Encontre um lugar que é a sua cara
         </h2>
@@ -14,28 +16,36 @@ export const PlacesSection = () => {
         </p>
       </div>
 
-      <Topic
-        buttonAction={() => {}}
-        buttonText="Ver imóveis para Alugar"
-        firstLinkPath="/"
-        firstLinkText="Veja como Alugar sem Fiador"
-        secondLinkPath="/"
-        secondLinkText="Saiba como Alugar na Netimóveis"
-        subtitle="Alugue imóveis qualificados e sem repitição. Sed ut perspiciatis unde omnis iste natus error sit."
-        title="Alugue sem Complicação"
-      />
+      <div className="px-4">
+        <Topic
+          buttonAction={() => {}}
+          buttonText="Ver imóveis para Alugar"
+          firstLinkPath="/"
+          firstLinkText="Veja como Alugar sem Fiador"
+          secondLinkPath="/"
+          secondLinkText="Saiba como Alugar na Netimóveis"
+          subtitle="Alugue imóveis qualificados e sem repitição. Sed ut perspiciatis unde omnis iste natus error sit."
+          title="Alugue sem Complicação"
+        />
+      </div>
 
-      <Topic
-        buttonAction={() => {}}
-        buttonText="Ver imóveis para Comprar"
-        firstLinkPath="/"
-        firstLinkText="Descubra seu Poder de Compra"
-        secondLinkPath="/"
-        secondLinkText="Saiba como Comprar na Netimóveis"
-        subtitle="Compre seu imóvel com segurança e agilidade. Sed ut perspiciatis unde
+      <div className="px-4">
+        <Topic
+          buttonAction={() => {}}
+          buttonText="Ver imóveis para Comprar"
+          firstLinkPath="/"
+          firstLinkText="Descubra seu Poder de Compra"
+          secondLinkPath="/"
+          secondLinkText="Saiba como Comprar na Netimóveis"
+          subtitle="Compre seu imóvel com segurança e agilidade. Sed ut perspiciatis unde
         omnis iste natus error sit."
-        title="Compre com Segurança"
-      />
+          title="Compre com Segurança"
+        />
+      </div>
+
+      <Separator className="border border-[#E1E5E9]" />
+
+      <Tabs></Tabs>
     </div>
   );
 };
