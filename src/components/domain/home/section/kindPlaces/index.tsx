90ae4d7fbad13469afc3956d3bfdb180982a7b3f
Harysson <haryssonsoares@gmail.com>
2024-01-12 20:35:29 -0300

    improvment: add kind places section on home page

---
+++ src/components/domain/home/section/kindPlaces/index.tsx
@@ -0,0 +1,47 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import React from "react";
+import { Content } from "./content";
+
+export const KindSection = () => {
+  return (
+    <div className="px-4">
+      <Tabs defaultValue="kind">
+        <TabsList className="h-full w-full p-0">
+          <Carousel className="relative w-full">
+            <CarouselContent>
+              <CarouselItem className="basis-2/5">
+                <TabsTrigger className="flex flex-col text-xs" value="kind">
+                  Tipos de Propriedades
+                </TabsTrigger>
+              </CarouselItem>
+              <CarouselItem className="basis-2/5">
+                <TabsTrigger className="flex flex-col text-xs" value="sell">
+                  Populares para Venda
+                </TabsTrigger>
+              </CarouselItem>
+              <CarouselItem className="basis-2/5">
+                <TabsTrigger className="flex flex-col text-xs" value="rent">
+                  Populares para Locação / Planta
+                </TabsTrigger>
+              </CarouselItem>
+            </CarouselContent>
+          </Carousel>
+        </TabsList>
+        <TabsContent value="kind" className="mt-0 flex flex-col gap-4">
+          <Content />
+        </TabsContent>
+        <TabsContent value="sell" className="mt-0 flex flex-col gap-4">
+          <Content />
+        </TabsContent>
+        <TabsContent value="rent" className="mt-0 flex flex-col gap-4">
+          <Content />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+};
