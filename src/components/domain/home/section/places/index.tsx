317db6decedf37f041bb8c5d67fc00f52d77e587
Harysson <haryssonsoares@gmail.com>
2024-01-12 11:40:16 -0300

    refactor: component to carousel places

--- src/components/domain/home/section/places/index.tsx
+++ src/components/domain/home/section/places/index.tsx
@@ -19,6 +19,7 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import Lottie from "lottie-react";
+import { CarouselPlaces } from "./caousel";
 
 export const PlacesSection = () => {
   return (
@@ -104,70 +105,7 @@ export const PlacesSection = () => {
         className="h-[192px] w-full object-cover object-top"
       />
 
-      <div className="relative grid grid-cols-3 gap-y-6 px-4">
-        <p className="col-span-2 font-bold text-[#4E5254]">
-          Prontos para te Receber em mais de 10 estados
-        </p>
-
-        <div className="col-span-3">
-          <Carousel>
-            <CarouselContent>
-              {places.map((place) => (
-                <CarouselItem
-                  key={place.placeId}
-                  className="flex basis-4/5 flex-col gap-6"
-                >
-                  {place.imageUrl ? (
-                    <Card className="relative overflow-hidden">
-                      <Image
-                        alt={`place image ${place.name}`}
-                        src={place.imageUrl}
-                        height={0}
-                        width={0}
-                        priority
-                        sizes="100vw"
-                        className="h-[192px] w-full object-cover object-top"
-                      />
-                      <p className="absolute bottom-4 left-4 text-2xl font-bold text-white">
-                        {place.name}
-                      </p>
-                    </Card>
-                  ) : (
-                    <div className="relative h-[192px] rounded-lg bg-[#ED672B]">
-                      <Lottie
-                        animationData={emptyImage}
-                        className="h-[150px]"
-                        loop={true}
-                      />
-                      <p className="absolute bottom-4 left-4 text-2xl font-bold text-white">
-                        {place.name}
-                      </p>
-                    </div>
-                  )}
-                  <ul className="flex list-inside list-disc flex-col gap-2 marker:text-[#ED672B]">
-                    <li className="text-sm text-[#4E5254]">
-                      Casas à venda em {place.name}
-                    </li>
-                    <li className="text-sm text-[#4E5254]">
-                      Apto. à venda em {place.name}
-                    </li>
-                    <li className="text-sm text-[#4E5254]">
-                      Casas para Locação em {place.name}
-                    </li>
-                    <li className="text-sm text-[#4E5254]">
-                      Apto. para locação em {place.name}
-                    </li>
-                  </ul>
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-            <div className="absolute right-12 top-[-50px] flex ">
-              <CarouselNext />
-              <CarouselPrevious />
-            </div>
-          </Carousel>
-        </div>
-      </div>
+      <CarouselPlaces />
     </div>
   );
 };
