1ed5e3731e74daa6587b8b9bedb6e4be66706545
Harysson <haryssonsoares@gmail.com>
2024-01-16 22:01:22 -0300

    improvment: heigth of banner and text position

--- src/components/domain/home/section/banner.tsx
+++ src/components/domain/home/section/banner.tsx
@@ -10,9 +10,9 @@ export const Banner = () => {
         height={0}
         priority
         sizes="100vw"
-        className="h-[173px] w-full object-cover"
+        className="h-[173px] w-full object-cover lg:h-[640px] lg:object-right"
       />
-      <div className="absolute left-16 top-[40px] flex flex-col">
+      <div className="absolute left-16 top-[40px] flex flex-col lg:left-1/2 lg:top-32 lg:-translate-x-1/2 lg:transform lg:flex-row">
         <p className="text-2xl font-bold uppercase text-white">VENDE.</p>
         <p className="text-2xl font-bold uppercase text-white">ALUGA.</p>
         <p className="text-2xl font-bold uppercase text-white">CONECTA.</p>
