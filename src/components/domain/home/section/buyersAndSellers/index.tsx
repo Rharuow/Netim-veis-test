cff80a622d70ddbd782c884e6c6c72b091084689
Harysson <haryssonsoares@gmail.com>
2024-01-18 21:36:05 -0300

    improvment: add responsive desktop media in buyers and sellers section

--- src/components/domain/home/section/buyersAndSellers/index.tsx
+++ src/components/domain/home/section/buyersAndSellers/index.tsx
@@ -5,8 +5,11 @@ import { Notice } from "./notice";
 
 export const BuyersAndSellersSection = () => {
   return (
-    <div className="flex flex-col gap-10 px-4 pt-20" id="buyersAndSellers">
-      <div className="flex flex-col gap-4">
+    <div
+      className="flex flex-col gap-10 px-4 pt-20 lg:w-[65%] lg:self-center"
+      id="buyersAndSellers"
+    >
+      <div className="flex flex-col gap-4 lg:flex-row lg:justify-between">
         <h1 className="text-xl font-bold text-[#4E5254]">
           Para compradores, vendedores ou curiosos :)
         </h1>
@@ -20,27 +23,29 @@ export const BuyersAndSellersSection = () => {
         </Link>
       </div>
 
-      <Notice
-        alt="meet banner"
-        minutesToRead={2}
-        src="/meet.png"
-        tag="Mundo Netimóveis"
-        title="ENCONTRO NETIMÓVEIS BRASIL – N CONNECTIONS 2021: A FORÇA DOS NEGÓCIOS EM REDE"
-      />
-      <Notice
-        alt="financial banner"
-        minutesToRead={2}
-        src="/financial.png"
-        tag="Finanças"
-        title="ORGANIZAÇÃO FINANCEIRA NAS IMOBILIÁRIAS"
-      />
-      <Notice
-        alt="management banner"
-        minutesToRead={2}
-        src="/management.png"
-        tag="Sucesso Profissional"
-        title="EFICIÊNCIA NA GESTÃO DE CHAMADOS DE MANUTENÇÃO"
-      />
+      <div className="flex flex-col gap-10 lg:grid lg:grid-cols-3">
+        <Notice
+          alt="meet banner"
+          minutesToRead={2}
+          src="/meet.png"
+          tag="Mundo Netimóveis"
+          title="ENCONTRO NETIMÓVEIS BRASIL – N CONNECTIONS 2021: A FORÇA DOS NEGÓCIOS EM REDE"
+        />
+        <Notice
+          alt="financial banner"
+          minutesToRead={2}
+          src="/financial.png"
+          tag="Finanças"
+          title="ORGANIZAÇÃO FINANCEIRA NAS IMOBILIÁRIAS"
+        />
+        <Notice
+          alt="management banner"
+          minutesToRead={2}
+          src="/management.png"
+          tag="Sucesso Profissional"
+          title="EFICIÊNCIA NA GESTÃO DE CHAMADOS DE MANUTENÇÃO"
+        />
+      </div>
     </div>
   );
 };
