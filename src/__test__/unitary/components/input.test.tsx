43620cae90dc8a729f37c04191b1710580682650
Harysson <haryssonsoares@gmail.com>
2024-01-20 19:33:43 -0300

    feat: add test to render input component

--- src/__test__/unitary/components/input.test.tsx
+++ src/__test__/unitary/components/input.test.tsx
@@ -1,4 +1,13 @@
+import { Input } from "@/components/ui/input";
 import "@testing-library/jest-dom";
 import { render, screen, fireEvent } from "@testing-library/react";
 
-describe("Test button component", () => {});
+describe("Test input component", () => {
+  test("should render input with text test", () => {
+    render(<Input />);
+
+    const input = screen.getByRole("textbox");
+
+    return expect(input).toBeInTheDocument();
+  });
+});
