b815b027d52311d508b08ea529a9ffae56f1e2c4
Harysson <haryssonsoares@gmail.com>
2024-01-19 21:26:06 -0300

    feat: add test to card in onClick property

--- src/__test__/unitary/components/card.test.tsx
+++ src/__test__/unitary/components/card.test.tsx
@@ -1,6 +1,6 @@
 import { Card } from "@/components/ui/card";
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 
 describe("Test Card component", () => {
   test("should render Card with text test", () => {
@@ -17,5 +17,30 @@ describe("Test Card component", () => {
     const card = screen.getByText("test");
 
     return expect(card).toHaveClass("testClassName");
+  });
+
+  test("should render Card with text test and call the provided onClick handler when clicked", () => {
+    const consoleSpy = jest.spyOn(console, "log");
+    render(
+      <Card
+        onClick={() => {
+          console.log("on click triggered");
+        }}
+      >
+        test
+      </Card>,
+    );
+
+    // Find the card using its text content
+    const card = screen.getByText("test");
+
+    // Simulate a click event on the card
+    fireEvent.click(card);
+
+    // Assert that console.log has been called with the correct message
+    expect(consoleSpy).toHaveBeenCalledWith("on click triggered");
+
+    // Clean up the spy
+    consoleSpy.mockRestore();
   });
 });
