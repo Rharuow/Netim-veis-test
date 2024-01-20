988bb7f3e592e42589cdffa54f7bf1bd40d29142
Harysson <haryssonsoares@gmail.com>
2024-01-20 18:58:50 -0300

    feat: add test to render carousel item

--- src/__test__/unitary/components/carousel.test.tsx
+++ src/__test__/unitary/components/carousel.test.tsx
@@ -1,4 +1,4 @@
-import { Carousel } from "@/components/ui/carousel";
+import { Carousel, CarouselItem } from "@/components/ui/carousel";
 import "@testing-library/jest-dom";
 import { render, screen, fireEvent, within } from "@testing-library/react";
 
@@ -9,5 +9,27 @@ describe("Test Carousel component", () => {
     const carousel = screen.getByText("test");
 
     return expect(carousel).toBeInTheDocument();
+  });
+
+  test("should render Carousel with text test and the class containing 'relative'", () => {
+    render(<Carousel>test</Carousel>);
+
+    const carousel = screen.getByText("test");
+
+    return expect(carousel).toHaveClass("relative");
+  });
+
+  test("should render CarouselItem with text test and the class containing 'min-w-0 shrink-0 grow-0 basis-full'", () => {
+    render(
+      <Carousel>
+        <CarouselItem>test</CarouselItem>
+      </Carousel>,
+    );
+
+    const carouselItem = screen.getByText("test");
+
+    return expect(carouselItem).toHaveClass(
+      "min-w-0 shrink-0 grow-0 basis-full",
+    );
   });
 });
